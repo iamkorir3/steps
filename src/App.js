5915a16c21dc7684593efbc46efaf6d0cb5fb649
Iamkorir3 <korir5956@gmail.com>
2025-06-13 23:21:54 +0300

    working with state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,64 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { useState } from "react";
 
-function App() {
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <p>kor</p>
-      <p>cor</p>
+    <div>
+      <Steps />
+      <Steps />
     </div>
   );
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
-console.log(root);
+function Steps() {
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
 
-export default App;
+  const handPrevious = () => {
+    if (step > 1) setStep((s) => s - 1);
+  };
+  const handNext = () => {
+    if (step < 3) setStep((s) => s + 1);
+  };
+  return (
+    <div>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+
+          <p className="message">
+            step {step}: {messages[step - 1]}{" "}
+          </p>
+
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handPrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handNext}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
