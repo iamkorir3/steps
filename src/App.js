43b40a100f77a4f3ebb8aeade7876b57a20dd91f
iamkorir3 <korir5956@gmail.com>
2025-08-23 23:23:27 +0300

    test

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <div>
       <Steps />
-      <Steps />
+      {/* <Steps /> */}
     </div>
   );
 }
@@ -39,26 +39,40 @@ function Steps() {
             <div className={`${step >= 3 ? "active" : ""}`}>3</div>
           </div>
 
-          <p className="message">
-            step {step}: {messages[step - 1]}{" "}
-          </p>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
 
           <div className="buttons">
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handPrevious}
-            >
-              Previous
-            </button>
-            <button
-              style={{ backgroundColor: "#7950f2", color: "#fff" }}
-              onClick={handNext}
-            >
-              Next
-            </button>
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handPrevious}>
+              <span>👈</span>Previous
+            </Button>
+
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handNext}>
+              Next<span>👉</span>{" "}
+            </Button>
           </div>
         </div>
       )}
     </div>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </div>
+  );
+}
+
+function Button({ textColor, bgColor, onClick, children }) {
+  console.log(bgColor);
+  return (
+    <button
+      style={{ backgroundColor: bgColor, color: textColor }}
+      onClick={onClick}
+    >
+      {children}
+    </button>
   );
 }
---
+++ src/test.js
@@ -0,0 +1,49 @@
+function test() {
+    const [step, setStep] = useState(1);
+      const [isOpen, setIsOpen] = useState(true);
+    
+      const handPrevious = () => {
+        if (step > 1) setStep((s) => s - 1);
+      };
+      const handNext = () => {
+        if (step < 3) setStep((s) => s + 1);
+      };
+      return (
+        <div>
+          <button className="close" onClick={() => setIsOpen((is) => !is)}>
+            &times;
+          </button>
+    
+          {isOpen && (
+            <div className="steps">
+              <div className="numbers">
+                <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+                <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+                <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+              </div>
+    
+              <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+    
+              <div className="buttons">
+                <Button textColor="#fff" bgColor="#7950f2" onClick={handPrevious}>
+                  <span>👈</span>Previous
+                </Button>
+    
+                <Button textColor="#fff" bgColor="#7950f2" onClick={handNext}>
+                  Next<span>👉</span>{" "}
+                </Button>
+              </div>
+            </div>
+          )}
+        </div>
+      );
+    }
+    
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default test
