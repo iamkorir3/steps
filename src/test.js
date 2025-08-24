9cc4b964c59b512a39d7ce9f842cdc882c7835df
iamkorir3 <korir5956@gmail.com>
2025-08-24 23:20:11 +0300

    debug

--- src/test.js
+++ src/test.js
@@ -1,49 +1,44 @@
 function test() {
-    const [step, setStep] = useState(1);
-      const [isOpen, setIsOpen] = useState(true);
-    
-      const handPrevious = () => {
-        if (step > 1) setStep((s) => s - 1);
-      };
-      const handNext = () => {
-        if (step < 3) setStep((s) => s + 1);
-      };
-      return (
-        <div>
-          <button className="close" onClick={() => setIsOpen((is) => !is)}>
-            &times;
-          </button>
-    
-          {isOpen && (
-            <div className="steps">
-              <div className="numbers">
-                <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-                <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-                <div className={`${step >= 3 ? "active" : ""}`}>3</div>
-              </div>
-    
-              <StepMessage step={step}>{messages[step - 1]}</StepMessage>
-    
-              <div className="buttons">
-                <Button textColor="#fff" bgColor="#7950f2" onClick={handPrevious}>
-                  <span>👈</span>Previous
-                </Button>
-    
-                <Button textColor="#fff" bgColor="#7950f2" onClick={handNext}>
-                  Next<span>👉</span>{" "}
-                </Button>
-              </div>
-            </div>
-          )}
+  const [step, setStep] = useState(1);
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handPrevious = () => {
+    if (step > 1) setStep((s) => s - 1);
+  };
+  const handNext = () => {
+    if (step < 3) setStep((s) => s + 1);
+  };
+  return (
+    <div>
+      <button className="close" onClick={() => setIsOpen((is) => !is)}>
+        &times;
+      </button>
+
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+            <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+            <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+          </div>
+
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+
+          <div className="buttons">
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handPrevious}>
+              <span>👈</span>Previous
+            </Button>
+
+            <Button textColor="#fff" bgColor="#7950f2" onClick={handNext}>
+              Next<span>👉</span>{" "}
+            </Button>
+          </div>
         </div>
-      );
-    }
-    
-    return (
-        <div>
-            
-        </div>
-    )
+      )}
+    </div>
+  );
 }
 
-export default test
+return <div></div>;
+
+export default test;
